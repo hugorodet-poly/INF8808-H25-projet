c0f425d4d08bb48c87eede0c8bf2ba7b45fd8dd2
hugorodet-poly <hugo.rodet@polymtl.ca>
2025-04-14 17:39:27 -0400

    Commented out sec 1

--- app.py
+++ app.py
@@ -70,11 +70,11 @@ app.layout = html.Div([
     # Main container
     html.Div([
         # 1. Immigration Map (Montreal)
-        html.Div([
-            html.H2('Immigration Distribution in Montreal', className='section-title'),
-            html.P('This map shows the percentage of immigrants across different electoral districts in Montreal.'),
-            dcc.Graph(figure=immigrants_map_fig, className='graph')
-        ], className='card'),
+        # html.Div([
+        #     html.H2('Immigration Distribution in Montreal', className='section-title'),
+        #     html.P('This map shows the percentage of immigrants across different electoral districts in Montreal.'),
+        #     dcc.Graph(figure=immigrants_map_fig, className='graph')
+        # ], className='card'),
 
         html.Hr(className='section-divider'),
 
