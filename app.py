eb92e3504db864402a3901f08973bdb10cffc45a
hugorodet-poly <hugo.rodet@polymtl.ca>
2025-04-14 21:12:20 -0400

    Added callback for fig 1

--- app.py
+++ app.py
@@ -77,7 +77,7 @@ app.layout = html.Div([
             
             html.Div(className='flex-row', children=[
                 html.Div(className='four columns', children=[ # Montreal Map
-                    dcc.Graph(id='montreal-immigrants-map', figure=montreal_boroughs_map, style={'justify': 'center'})]),
+                    dcc.Graph(id='boroughs-immigrants-map', figure=montreal_boroughs_map, style={'justify': 'center'})]),
                 html.Div(className='eight columns', children=[ # World map
                     dcc.Graph(id='world-immigrants-map', style={'justify': 'center'})])]),
         ], className='card'),
@@ -86,7 +86,7 @@ app.layout = html.Div([
         html.Div([
             html.H2('Immigration Distribution in Montreal', className='section-title'),
             html.P('This map shows the percentage of immigrants across different electoral districts in Montreal.'),
-            dcc.Graph(figure=immigrants_map_fig, className='graph')
+            dcc.Graph(id='montreal-immigrants-map', className='graph')
         ], className='card'),
 
         html.Hr(className='section-divider'),
@@ -219,12 +219,18 @@ def update_party_income_chart(party):
     return dcc.Graph(figure=fig, className='graph')
 
 @app.callback(
-    Output(component_id='world-immigrants-map', component_property='figure'),
+    Output(component_id='boroughs-immigrants-map', component_property='figure'),
     Output(component_id='current-borough', component_property='children'),
     Input(component_id='montreal-immigrants-map', component_property='clickData'))
 def update_world_immigrants_map(clickdata):
     fig, borough = get_world_immigrants_map(montreal_boroughs_mapdata, world_mapdata, borough_df, clickdata)
     return fig, borough
+
+@app.callback(
+    Output(component_id='montreal-immigrants-map', component_property='figure'),
+    Input(component_id='current_borough', component_property='children'))
+def update_montreal_immigrants_map(void):
+    return immigrants_map_fig
 
 if __name__ == '__main__':
     app.run(debug=False)
